aa45ef49d5cfe39303b1dcad7a18aedd2e0fa46d
Caique Moreira <caiquemoreiradev@gmail.com>
2022-02-03 17:01:41 -0300

    challenge-done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import '../styles/header.scss'
 export function Header() {
   return (
     <header className="header">
-      <div>
+        <img className='ignite-logo' src="/ignite.svg" alt="Ignite" />
         <img src="/logo.svg" alt="to.do"/>
-      </div>
     </header>
   )
 }
